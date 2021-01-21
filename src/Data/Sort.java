31b027e457bda89cb9d64c06a79021faa205dce1
Kylemendes21 <you@example.com>
2021-01-20 22:47:54 -0500

    create sort function

--- src/Data/Sort.java
+++ src/Data/Sort.java
@@ -2,4 +2,51 @@ package Data;
 
 public class Sort {
     
+    public static Movies[] sortMin(Movies[] array) {
+
+        int currentMinIndex;
+
+        for(int i = 0; i < array.length - 1; i++) {
+            currentMinIndex = i;
+
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[j].getTotalgross() < array[currentMinIndex].getTotalgross()) {
+                    currentMinIndex = j;
+                }
+            }
+            
+        }
+
+        if(i != currentMinIndex) {
+            Movies temp = array[currentMinIndex];
+            array[currentMinIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    return array;
+
+    public static Movies[] sortMax(Movies[] array) {
+
+        int currentMinIndex;
+
+        for(int i = 0; i < array.length - 1; i++) {
+            currentMinIndex = i;
+
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[j].getTotalgross() > array[currentMinIndex].getTotalgross()) {
+                    currentMinIndex = j;
+                }
+            }
+            
+        }
+
+        if(i != currentMinIndex) {
+            Movies temp = array[currentMinIndex];
+            array[currentMinIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    return array;
 }
